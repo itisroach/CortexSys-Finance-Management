1c94d2ba9e10a6099134e7c47de32d7c9127d4cf
unknown <rouchashoori@gmail.com>
2025-08-16 17:44:25 +0330

    Add serializer for transactions

---
+++ transactions/serializers.py
@@ -0,0 +1,25 @@
+from rest_framework import serializers
+from .models import Transaction
+
+
+class TransactionSerializer(serializers.ModelSerializer):
+
+    date = serializers.DateField("%Y-%m-%d")
+
+    class Meta:
+
+        model = Transaction
+        fields = ["title", "amount","type", "date", "notes"]
+
+
+    def create(self, validated_data):
+
+        request = self.context.get("request")
+
+        user = request.user
+
+        validated_data["user_id"] = user
+
+        transaction = Transaction.objects.create(**validated_data)
+        return transaction
+    
