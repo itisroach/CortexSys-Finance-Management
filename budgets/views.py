df437f8ebbbfd81e120473cce06b8e2188f8c70f
unknown <rouchashoori@gmail.com>
2025-08-17 02:13:46 +0330

    Add model viewset for handling CRUD operation on budget

--- budgets/views.py
+++ budgets/views.py
@@ -1,3 +1,21 @@
 from django.shortcuts import render
+from rest_framework.viewsets import ModelViewSet
+from .serializers import BudgetSerializer
+from rest_framework.permissions import IsAuthenticated
+from .models import Budget
 
-# Create your views here.
+
+
+class BudgetViewset(ModelViewSet):
+
+    serializer_class = BudgetSerializer
+    permission_classes = [IsAuthenticated,]
+    queryset = Budget.objects.all()
+
+
+    def get_queryset(self):
+        queryset = super().get_queryset()
+
+        queryset = Budget.objects.filter(user_id=self.request.user.id)
+
+        return queryset
