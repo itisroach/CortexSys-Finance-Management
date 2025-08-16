bc85a5ca66cd7706f3c52a40a5c5f6873a21b78b
unknown <rouchashoori@gmail.com>
2025-08-16 14:50:13 +0330

    Add custom user model

--- accounts/models.py
+++ accounts/models.py
@@ -1,3 +1,43 @@
+# users/models.py
+from django.contrib.auth.models import AbstractBaseUser, BaseUserManager, PermissionsMixin
 from django.db import models
+from django.utils import timezone
 
-# Create your models here.
+class UserManager(BaseUserManager):
+    def create_user(self, phone_number, password=None, name=None, **extra_fields):
+        if not phone_number:
+            raise ValueError('The Phone Number must be set')
+        phone_number = str(phone_number)
+        user = self.model(phone_number=phone_number, name=name, **extra_fields)
+        user.set_password(password)
+        user.save(using=self._db)
+        return user
+
+    def create_superuser(self, phone_number, password=None, name=None, **extra_fields):
+        extra_fields.setdefault('is_staff', True)
+        extra_fields.setdefault('is_superuser', True)
+        extra_fields.setdefault('is_active', True)
+
+        if extra_fields.get('is_staff') is not True:
+            raise ValueError('Superuser must have is_staff=True.')
+        if extra_fields.get('is_superuser') is not True:
+            raise ValueError('Superuser must have is_superuser=True.')
+
+        return self.create_user(phone_number, password, name, **extra_fields)
+
+
+class Account(AbstractBaseUser, PermissionsMixin):
+    id = models.BigAutoField(primary_key=True)
+    phone_number = models.CharField(max_length=20, unique=True)
+    name = models.CharField(max_length=255, blank=True, null=True)
+    is_active = models.BooleanField(default=True)
+    is_staff = models.BooleanField(default=False)
+    date_joined = models.DateTimeField(default=timezone.now)
+
+    objects = UserManager()
+
+    USERNAME_FIELD = 'phone_number'
+    REQUIRED_FIELDS = [] 
+
+    def __str__(self):
+        return self.phone_number
