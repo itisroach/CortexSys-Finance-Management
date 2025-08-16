104b9916463a14471ff57374c82df487f41594bd
unknown <rouchashoori@gmail.com>
2025-08-16 17:44:59 +0330

    Add url of viewsets to transactions url app

--- transactions/views.py
+++ transactions/views.py
@@ -1,3 +1,16 @@
 from django.shortcuts import render
+from rest_framework import viewsets
+from .models import Transaction
+from .serializers import TransactionSerializer
+from rest_framework.permissions import IsAuthenticated
 
-# Create your views here.
+class TransactionsView(viewsets.ModelViewSet):
+
+    serializer_class = TransactionSerializer
+    permission_classes = (IsAuthenticated,)
+    queryset = Transaction.objects.all()
+
+    def get_queryset(self):
+        queryset = super().get_queryset()
+
+        queryset = queryset.filter(user_id=self.request.user.id)
