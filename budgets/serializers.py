50b54510810cff6392917eb9cf5de1441f61ce11
unknown <rouchashoori@gmail.com>
2025-08-17 02:29:49 +0330

    Add validation for end_date in budget

--- budgets/serializers.py
+++ budgets/serializers.py
@@ -24,3 +24,12 @@ class BudgetSerializer(serializers.ModelSerializer):
         budget = Budget.objects.create(**validated_data)
         return budget
     
+    def validate(self, data):
+        start_date = data.get('start_date')
+        end_date = data.get('end_date')
+
+        if start_date and end_date and end_date < start_date:
+            raise serializers.ValidationError(
+                {"end_date": "End date cannot be earlier than start date."}
+            )
+        return data
