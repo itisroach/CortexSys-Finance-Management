154556aebeaa41e65b67fdaff391d14d4644d62a
unknown <rouchashoori@gmail.com>
2025-08-16 17:43:55 +0330

    Add model for transactions

--- transactions/models.py
+++ transactions/models.py
@@ -1,3 +1,23 @@
 from django.db import models
+from accounts.models import Account
+from django.core.validators import MinValueValidator
 
-# Create your models here.
+
+class Transaction(models.Model):
+
+    typeChoices = (
+        ("income", "income"),
+        ("expense", "expense")
+    )
+
+    user_id = models.ForeignKey(Account, on_delete=models.CASCADE)
+
+    title = models.CharField(max_length=255, blank=False, null=False)
+    amount = models.BigIntegerField(blank=False, null=False, validators=[MinValueValidator(1)])
+    
+    type = models.CharField(max_length=10, choices=typeChoices, blank=False, null=False)
+
+    date = models.DateField(blank=False, null=False)
+
+    notes = models.TextField(blank=True, null=True)
+
