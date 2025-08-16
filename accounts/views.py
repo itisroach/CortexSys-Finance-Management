16b3062d71d5478ffa897ff82d93ac9974f34e57
unknown <rouchashoori@gmail.com>
2025-08-16 14:49:36 +0330

    Add register view

--- accounts/views.py
+++ accounts/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
+from rest_framework.generics import CreateAPIView 
+from .serializers import AccountSerializer
+from accounts.models import Account
+from rest_framework.permissions import AllowAny
 
-# Create your views here.
+class RegisterView(CreateAPIView):
+    serializer_class = AccountSerializer
+    queryset = Account.objects.all()
+    permission_classes = [AllowAny]
+    
