494c1a9d6a9b1e132cd810b3b8abc0c70e4a2e95
unknown <rouchashoori@gmail.com>
2025-08-17 02:14:56 +0330

    Add urls of budget app to project

--- finance_management/urls.py
+++ finance_management/urls.py
@@ -5,4 +5,5 @@ urlpatterns = [
     path("admin/", admin.site.urls),
     path("api/auth/", include("accounts.urls")),
     path("api/", include("transactions.urls")),
+    path("api/", include("budgets.urls")),
 ]
