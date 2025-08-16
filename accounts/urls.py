a74f78b49dcf3fc906567efa0a3831241d9319b2
unknown <rouchashoori@gmail.com>
2025-08-16 15:20:53 +0330

    Add login route to app's urls

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
 from . import views
+from rest_framework_simplejwt.views import TokenObtainPairView
 
 
 urlpatterns = [
-    path("register/", views.RegisterView.as_view(), name="register-user")
+    path("register/", views.RegisterView.as_view(), name="register-user"),
+    path("login/", TokenObtainPairView.as_view(), name="login-user")
 ]
