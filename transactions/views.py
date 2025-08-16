5285fdb214f9b353e68c2f5b461a8c966c137a65
unknown <rouchashoori@gmail.com>
2025-08-16 23:54:55 +0330

    Add report view for generating simple report and also add query param for getting specific type of transactions

--- transactions/views.py
+++ transactions/views.py
@@ -1,7 +1,8 @@
-from django.shortcuts import render
 from rest_framework import viewsets
 from .models import Transaction
 from .serializers import TransactionSerializer
+from rest_framework.decorators import action
+from rest_framework.response import Response
 from rest_framework.permissions import IsAuthenticated
 
 class TransactionsView(viewsets.ModelViewSet):
@@ -11,8 +12,29 @@ class TransactionsView(viewsets.ModelViewSet):
     queryset = Transaction.objects.all()
 
     def get_queryset(self):
+
+        query = self.request.query_params.get("type")
+            
         queryset = super().get_queryset()
 
-        queryset = Transaction.objects.filter(user_id=self.request.user.id)
+        if not query:
+            queryset = Transaction.objects.filter(user_id=self.request.user.id)
+
+        else:
+            queryset = Transaction.objects.filter(user_id=self.request.user.id, type=query)
 
         return queryset
+    
+
+    @action(detail=False, methods=['get'])
+    def report(self, request):
+        user = request.user
+        balance, income, expense = user.balance()
+
+
+
+        return Response({
+            'total_income': income,
+            'total_expense': expense,
+            'balance': balance,
+        })
