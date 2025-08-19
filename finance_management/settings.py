8559d97fddcd9e75a90a8ec48a078d386aa9bb75
unknown <rouchashoori@gmail.com>
2025-08-20 00:36:05 +0330

    Update database settings

--- finance_management/settings.py
+++ finance_management/settings.py
@@ -65,7 +65,11 @@ TEMPLATES = [
 WSGI_APPLICATION = "finance_management.wsgi.application"
 
 
-DATABASES = {"default": dj_database_url.config(default=os.environ.get("DATABASE_URL"))}
+DATABASES = {
+    "default": dj_database_url.config(
+        default=os.environ.get("DATABASE_URL"), conn_max_age=600, ssl_require=False
+    )
+}
 
 
 # Password validation
