ff22bdc274440ba4b5d7f115e46765e4c2e67b46
unknown <rouchashoori@gmail.com>
2025-08-19 22:54:14 +0330

    Add view for CRUD operations on device tokens

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,10 +1,14 @@
 from django.urls import path
+from rest_framework.routers import DefaultRouter
 from . import views
 from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
+from accounts.views import DeviceTokenViewSet
 
+router = DefaultRouter()
+router.register(r"device-tokens", DeviceTokenViewSet, basename="device-token")
 
 urlpatterns = [
     path("register/", views.RegisterView.as_view(), name="register-user"),
     path("login/", TokenObtainPairView.as_view(), name="login-user"),
     path("refresh/", TokenRefreshView.as_view(), name="refresh-token"),
-]
+] + router.urls
